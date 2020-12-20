489a846d4b51589b7a2f459b8c3d8cfc4585bc74
andrewkyntsevich <mr.foxik96>
2020-12-20 15:29:30 +0300

    translation block created

--- src/index.js
+++ src/index.js
@@ -4,11 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
+
+import en from "./lang/en/en.json";
+import ru from "./lang/ru/ru.json";
+
+i18next.init({
+    interpolation: { escapeValue: false },  // React already does escaping
+    lng: 'en',                              // language to use
+    resources: {
+        en: {
+            lang: en               // 'common' is our custom namespace
+        },
+        ru: {
+            lang: ru
+        },
+    },
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <I18nextProvider i18n={i18next}>
+            <App/>
+        </I18nextProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
