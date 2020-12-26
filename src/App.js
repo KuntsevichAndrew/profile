7fe9dc07d382cb0d0aa5256f56def3e84ad66718
andrewkyntsevich <mr.foxik96>
2020-12-26 12:39:30 +0300

    about component created

--- src/App.js
+++ src/App.js
@@ -8,27 +8,31 @@ import Experience from "./body/experience/Experience";
 import Skill from "./body/skills/Skill";
 import Project from "./body/projects/Project";
 import Contact from "./body/contacts/Contact";
+import {Container, Grid, Typography} from "@material-ui/core";
+import ReactTypingEffect from "react-typing-effect";
+import MeImage from "./assets/image/me.jpg";
 
 
 function App() {
-  return (
-      <div className="App">
-
-          <Router>
-              <Header/>
-
-              <Route exact path="/" component={About}/>
-              <Route exact path="/about" component={About}/>
-              <Route exact path="/experience" component={Experience}/>
-              <Route exact path="/skill" component={Skill}/>
-              <Route exact path="/project" component={Project}/>
-              <Route exact path="/contact" component={Contact}/>
+    return (
 
 
-          </Router>
-          <Footer/>
-      </div>
-  );
+
+            <Router>
+                <Header/>
+                <div className="App">
+                <Route exact path="/" component={About}/>
+                <Route exact path="/about" component={About}/>
+                <Route exact path="/experience" component={Experience}/>
+                <Route exact path="/skill" component={Skill}/>
+                <Route exact path="/project" component={Project}/>
+                <Route exact path="/contact" component={Contact}/>
+                </div>
+                <Footer/>
+            </Router>
+
+
+    );
 }
 
 export default App;
--- src/body/about/About.js
+++ src/body/about/About.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import { withTranslation } from "react-i18next";
-import { Container, Typography, Grid } from "@material-ui/core";
+import {withTranslation} from "react-i18next";
+import {Container, Typography, Grid} from "@material-ui/core";
 import ReactTypingEffect from 'react-typing-effect';
 
 import "../../assets/style/about.css";
@@ -8,24 +8,23 @@ import MeImage from "../../assets/image/me.jpg";
 
 class About extends Component {
     render() {
-        const { t } = this.props;
+        const {t} = this.props;
         return (
             <Container maxWidth="lg" className="about-container">
-                <Grid item justify="center"
-                      alignItems="center" xs={12} className="about-title">
-                    <ReactTypingEffect text={t("about.title")} /></Grid>
+                <Grid item className="about-title" md={12} lg={12} xs={12}>
+                    <ReactTypingEffect text={t("about.title")}/></Grid>
 
                 <Grid container justify="center"
-                      alignItems="center" spacing={3}>
-                    <Grid item xs={4}>
-                        <img src={MeImage} className="my-image" />
+                      alignItems="center" direction="row" md={12} lg={12} xs={12} spacing={3}>
+                    <Grid item md={4} lg={4} xs={4}>
+                        <img src={MeImage} className="my-image"/>
                     </Grid>
-                    <Grid item xs={8}>
-
+                    <Grid item md={8} lg={8} xs={8}>
                         <Typography className="desc-description">{t("about.description")}</Typography>
                     </Grid>
                 </Grid>
             </Container>
+
             // <Grid
             //     container
             //     direction="row"
